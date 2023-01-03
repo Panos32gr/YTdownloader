df5e0c8c002f9b2109117988c6e939cce6613f77
Ambraxis <Namkorr@gmail.com>
2023-01-03 16:14:05 +0200

    First Commit.

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template, session
+from pytube import YouTube
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "aaaa"
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        session['link'] = request.form.get('url')
+        try:
+            url = YouTube(session['link'])
+            url.check_availability()
+            video = url.streams.get_highest_resolution()
+            video.download(output_path="C:/Users/Panos/Downloads")
+        except:
+            return render_template("error.html")
+        return render_template("download.html", url=url)
+    return render_template("home.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
